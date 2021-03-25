56611421c3be8f3d2f153850439a6cde67ca9bc6
apavani03 <76710296+apavani03@users.noreply.github.com>
2021-03-24 19:58:30 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function getParagraph1(){
+    var inputs=[]
+    inputs.join(" . ")
+    document.getElementById("showParagraph1").innerHTML=inputs.join(" . ");
+    
+    }
+
+    function getParagraph2(){
+        var inputs=[]
+        inputs.join(" . ")
+        document.getElementById("showParagraph2").innerHTML=inputs.join(" . ");
+        
+        }
