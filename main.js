d076a2f40f97cf055c30aac5bb92db1bf882f5cf
apavani03 <76710296+apavani03@users.noreply.github.com>
2021-03-25 18:24:40 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function getParagraph1(){
     var inputs1=[];
-    for(var i=1; i<=6; i++;){
+    for(var i=1; i<=6; i++){
     inputs1.push(document.getElementById("para1_input_"+i).value);
     }
     inputs1.join(" . ");
@@ -10,8 +10,9 @@ function getParagraph1(){
 
     function getParagraph2(){
         var inputs2=[];
-            for(var i=1; i<=6; i++;){
+            for(var i=1; i<=6; i++){
     inputs2.push(document.getElementById("para2_input_"+i).value);
+            }
         inputs2.join(" . ");
         document.getElementById("showParagraph2").innerHTML=inputs2.join(" . ");
         
